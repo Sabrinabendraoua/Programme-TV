633b77ef45512cc5f363de28fbe25d37b0b207b8
sabrina bendraoua <sabbendra@gmail.com>
2023-04-25 20:49:24 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Header from "./components/Header";
+import Data from "../src/data.json";
+
+function App() {
+  const data = Data;
+  console.log(data);
+  return (
+    <div className="container">
+      <Header />
+      {data.map((programmeTV) => {
+        console.log(programmeTV);
+        return (
+          <section>
+            <div>{programmeTV.time}</div>
+            <img src={programmeTV.image} />
+            <div>
+              <h2>{programmeTV.title}</h2>
+              <h3>{programmeTV.type}</h3>
+              <div>
+                <p>{programmeTV.duration}</p>
+                <p className="direct">
+                  {programmeTV.direct ? "direct" : "inédit"}
+                </p>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>M6</h1>
+    </header>
+  );
+}
+
+export default Header;
